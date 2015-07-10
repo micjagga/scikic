1804466a0158731a63c5d83412ae1e2ec3163ae2
fswatts <fswatts@msn.com>
2015-07-10 11:09:53 +0100

    NOw integrated with api, It takes inputs and returns insights

--- answer_music.py
+++ answer_music.py
@@ -12,17 +12,28 @@ import sqlalchemy as sa
 import pandas as pd
 import helper_functions as hf
 import config
+import sys
+import random
 
 from StringIO import StringIO
 from zipfile import ZipFile
 
+#global event_names
+#global event_datetimes
+#global event_locations
+#global event_listings
+#event_listings = []
+#event_names = []
+#event_datetimes = []
+#event_locations = []
+
 
 class MusicAnswer(ans.Answer):
-    """Movielens answer: handles seen, ratings, etc associated with movie rankings"""
-    
+    """"MusicAnswer takes an input of an artist the user likes and returns a similar artist and a recommended local event based on the tase of the user."""
     dataset = 'music';
 
-    def insights(self,inference_result,facts):        
+    def insights(self,inference_result,facts):  
+        limit = 10      
         #returns a list of insights:
         # - inference_result: probability distributions of the features
         # - facts: a dictionary of 'facts' provided by the Answer classes
@@ -33,9 +44,78 @@ class MusicAnswer(ans.Answer):
         #Their location will be in...
             #facts['city'] maybe???
             #facts['country'] maaaaaybe?
-        insight_list = ["Why not check out Humbar who are playing in Sheffield next week!!","I think as you like Air, I think you'll like Royksopp.","More insights..."]
-        return insight_list
+        event_names = []  
+        event_datetimes = []
+        event_locations = []
         
+        artist = self.answer
+        artist = artist.replace(" ", "+")   
+        url_r = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=%s&limit=%i&autocorrect=1&api_key=02c3301ab9e648c504edf4d7dc93c99b" %(artist, limit)
+        page_r = urllib2.urlopen(url_r)
+        contents_r = str(page_r.read())    
+
+        r_r = ET.fromstring(contents_r)
+
+        global results_artist_name
+        results_artist_name = []
+        results_artist_url = []
+        
+        for artist in r_r.find('similarartists'):
+            results_artist_name.append(artist.find('name').text)    
+            results_artist_url.append(artist.find('url').text)
+       
+        event_listings = []
+        for artist in results_artist_name:
+             new_events = self.find_local_events(self.answer, facts['city'], facts['country'])
+             for event in new_events:
+                 found = False
+                 for e in event_listings:
+                     if (e['title']==event['title']):
+                         found = True
+                         break
+                 if found:
+                     continue
+     
+                 event_listings.append(event)
+   
+        if len(event_listings) == 0:
+            x = random.randint(0,len(results_artist_name))  
+            return ["Unfortunately there are no local events I think you'd like but why not give " + results_artist_name[x] + " a listen ?"]
+        else:
+            return [self.output_events(event_listings, results_artist_name)]
+
+    def find_local_events(self, artist, city, country):
+        artist_formatted = artist.replace(" ", "%20")
+        import urllib
+        try:
+            url_e = "http://api.bandsintown.com/artists/%s/events/recommended?format=xml&app_id=scikic&api_version=2.0&location=%s,%s&callback=showEvents" %(artist_formatted, city, country)
+            page_e = urllib2.urlopen(url_e)
+        except UnicodeEncodeError: #TODO Handle this
+            return []
+        contents_e = str(page_e.read())   
+        r_e = ET.fromstring(contents_e)
+        event_listings = []
+        for event in r_e:
+            title = event.find ('title').text
+            title = title[:title.find(' in ')]
+            datetime = event.find ('datetime').text
+            formatted_datetime = event.find ('formatted_datetime').text
+            location = event.find ('formatted_location').text
+            event = ({  "location": location,"datetime": datetime,"formatted_datetime": formatted_datetime, "title": title})
+            event_listings.append(event)
+        return event_listings
+
+    def output_events(self,event_listings, results_artist_name):
+        insight_list = []
+        limit = 5    
+        event_listings = sorted(event_listings, key=lambda k: k['datetime'])
+        for i,event in enumerate(event_listings):
+            if i>=limit:
+                break   
+            x = random.randint(0,len(results_artist_name))      
+            insight_list.append(str("There's a " + event_listings[i]["title"][:((event_listings[i]["title"]).find(" @ "))] + " concert at " + event_listings[i]["title"][(((event_listings[i]["title"]).find(" @ "))+3):] + " " + event_listings[i]["location"] + " on "+ event_listings[i]["formatted_datetime"] +". If you can't make it why not give " + results_artist_name[x] + " a listen ?"))
+        return insight_list[0]
+    
 
     @classmethod
     def setup(cls,pathToData):
