59be8fcada16116a4c045a79b1d013fa9c9c5c1f
Enoc Leonrd <leonrdenoc@gmail.com>
2016-04-30 06:17:49 +0100

    ss

--- answer_uk_census.py
+++ answer_uk_census.py
@@ -225,16 +225,16 @@ class UKCensusAnswer(ans.Answer):
         trans_type = UKCensusAnswer.transport_text[np.argmax(localratios)]
         insights['ukcensus_traveltowork'] = 'People in your area are %0.0f times more likely to %s than the national average.' % (maxnum, trans_type)
         
-        bedrooms_probs = np.array([ 0.00244898, 0.11526287, 0.27649496, 0.41621374, 0.14389724, 0.04568222]);
+        # bedrooms_probs = np.array([ 0.00244898, 0.11526287, 0.27649496, 0.41621374, 0.14389724, 0.04568222]);
         
-        n_bedrooms_probs = bedrooms_probs + (1.0/200) 
-        n_bedrooms_probs = n_bedrooms_probs / np.sum(bedrooms_probs)
+        # n_bedrooms_probs = bedrooms_probs + (1.0/200) 
+        # n_bedrooms_probs = n_bedrooms_probs / np.sum(bedrooms_probs)
 
-        lr = self.household_bedrooms_probs/n_bedrooms_probs
-        mn = np.max(lr)
-        bedroom_type = UKCensusAnswer.bedrooms_text[np.argmax(lr)]
-        logging.info(bedroom_type, mn, '---------->')
-        insights['ukcensus_household_bedrooms'] = 'Households in your area have %0.0f times more likely to %s than the national average.' % (mn, bedroom_type)
+        # lr = self.household_bedrooms_probs/n_bedrooms_probs
+        # mn = np.max(lr)
+        # bedroom_type = UKCensusAnswer.bedrooms_text[np.argmax(lr)]
+        # logging.info(bedroom_type, mn, '---------->')
+        # insights['ukcensus_household_bedrooms'] = 'Households in your area have %0.0f times more likely to %s than the national average.' % (mn, bedroom_type)
         
         
         
@@ -628,7 +628,7 @@ class UKCensusAnswer(ans.Answer):
         self.calc_probs_religion(facts)
         self.calc_probs_household(facts)
         self.calc_probs_travelToWork(facts) 
-        #self.calc_probs_household_bedrooms(facts)       
+        self.calc_probs_household_bedrooms(facts)       
         self.calc_probs_countryOfBirth(facts)        
         self.get_other_distributions(facts) #this isn't necessary here as these methods don't assist with the features.        
         
