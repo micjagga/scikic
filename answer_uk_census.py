1a2860f6c92c077cd07a770769b69a97ff34a7a1
Enoc Leonrd <leonrdenoc@gmail.com>
2016-05-03 15:13:06 +0100

    done

--- answer_uk_census.py
+++ answer_uk_census.py
@@ -241,16 +241,16 @@ class UKCensusAnswer(ans.Answer):
         logging.info(UKCensusAnswer.languages_text[np.argmax(self.languages)])
         insights['ukcensus_language_list'] = self.languages[0].tolist()
 
-        # bedroom_probs = np.array([0.00244898, 0.11526287, 0.27649496, 0.41621374, 0.14389724, 0.04568222])
-        # bedroom_probs = (bedroom_probs + (1.0 / 200)) / np.sum(bedroom_probs)
+        bedroom_probs = np.array([0.00244898, 0.11526287, 0.27649496, 0.41621374, 0.14389724, 0.04568222])
+        bedroom_probs = (bedroom_probs + (1.0 / 200)) / np.sum(bedroom_probs)
 
-        # lrat = self.bed_probs / bedroom_probs
-        # # maxno = np.max(lrat)
-        # bedroom_type = UKCensusAnswer.bedrooms_text[np.argmax(lrat)]
-        # insights['ukcensus_bedrooms'] = 'The Houses in your area are more likely to have %s on average' % (
-        #     bedroom_type)
-        # logging.info(self.bed_probs)
-        # logging.info(insights['ukcensus_bedrooms'])
+        lrat = self.bed_probs / bedroom_probs
+        # maxno = np.max(lrat)
+        bedroom_type = UKCensusAnswer.bedrooms_text[np.argmax(lrat)]
+        insights['ukcensus_bedrooms'] = 'The Houses in your area are more likely to have %s on average' % (
+            bedroom_type)
+        logging.info(self.bed_probs)
+        logging.info(insights['ukcensus_bedrooms'])
 
         return insights
 
@@ -384,18 +384,18 @@ class UKCensusAnswer(ans.Answer):
         arr = arr * 1.0
         returnList[0] = arr
 
-    # @classmethod
-    # def getBedroomsDist(cls, geoArea, returnList):
-    #     data, mat = cls.ONSapiQuery(geoArea, 'QS411EW')
-    #     arr, labs = dict_to_array(mat)  # Convert the dictionary hierarchy to a numpy array
-    #     order = [[i for i, l in enumerate(labs[0]) if l == r][0] for r in
-    #              cls.bedrooms]  # sort by the order we want it in.
-    #     arr = np.array(arr)  # convert to numpy array
-    #     arr = arr[order]
-    #     arr = arr * 1.0
-    #     arr += 1.0
-    #     arr = 1.0 * arr / np.sum(1.0 * arr)
-    #     returnList[0] = arr  # now return via the argument so this can be called as a thread
+    @classmethod
+    def getBedroomsDist(cls, geoArea, returnList):
+        data, mat = cls.ONSapiQuery(geoArea, 'QS411EW')
+        arr, labs = dict_to_array(mat)  # Convert the dictionary hierarchy to a numpy array
+        order = [[i for i, l in enumerate(labs[0]) if l == r][0] for r in
+                 cls.bedrooms]  # sort by the order we want it in.
+        arr = np.array(arr)  # convert to numpy array
+        arr = arr[order]
+        arr = arr * 1.0
+        arr += 1.0
+        arr = 1.0 * arr / np.sum(1.0 * arr)
+        returnList[0] = arr  # now return via the argument so this can be called as a thread
 
 
     def __init__(self,name,dataitem,itemdetails,answer=None):
@@ -525,14 +525,14 @@ class UKCensusAnswer(ans.Answer):
             self.age_probs[:,i,0] = 1-p
             self.age_probs[:,i,1] = p
 
-    # def calc_probs_bedrooms(self, facts):
-    #     # returns p(oa|bedrooms)
-    #     oas = self.get_list_of_oas(facts)
-    #     localDists = self.getDist(oas, UKCensusAnswer.getBedroomsDist)
-    #     shape = localDists[0].shape
-    #     self.bed_probs = np.empty((len(localDists), shape[0]))
-    #     for i, p in enumerate(localDists):
-    #         self.bed_probs[i, :] = p
+    def calc_probs_bedrooms(self, facts):
+        # returns p(oa|bedrooms)
+        oas = self.get_list_of_oas(facts)
+        localDists = self.getDist(oas, UKCensusAnswer.getBedroomsDist)
+        shape = localDists[0].shape
+        self.bed_probs = np.empty((len(localDists), shape[0]))
+        for i, p in enumerate(localDists):
+            self.bed_probs[i, :] = p
 
     def get_pymc_function_age(self,features):
         """Returns a function for use with the pyMC module:
@@ -657,7 +657,7 @@ class UKCensusAnswer(ans.Answer):
 
     @classmethod
     def pick_question(cls,questions_asked,facts,target):
-	    return 'None','agegender'
+        return 'None','agegender'
 
 
 
